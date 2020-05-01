5915257d3b1dc0191233d7de0a09524466ee10aa
joonassuo <joonas.suonpera@gmail.com>
2020-05-01 19:59:04 +0300

    uploaded  pictures are tied to publication in Publication Schema

--- models/publication.js
+++ models/publication.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const publicationSchema = new Schema({
+const PublicationSchema = new Schema({
+	creator: {
+		type: String,
+		required: true,
+	},
+	contactPerson: {
+		type: String,
+		required: true,
+	},
+	contactEmail: {
+		type: String,
+		required: true,
+	},
 	title: {
 		type: String,
 		required: true,
 	},
+	titlePicture: {
+		type: String,
+		required: false,
+	},
+	pictures: [String],
 });
+
+module.exports = Publication = mongoose.model("publication", PublicationSchema);
--- routes/api/upload.js
+++ routes/api/upload.js
@@ -6,6 +6,9 @@ const GridFsStorage = require("multer-gridfs-storage");
 const config = require("config");
 const path = require("path");
 
+// PUBLICATION MODEL
+const Publication = require("../../models/publication");
+
 // CREATE STORAGE ENGINE
 const storage = new GridFsStorage({
 	url: config.get("MONGODB_URI"),
@@ -29,16 +32,33 @@ const storage = new GridFsStorage({
 const upload = multer({ storage });
 
 // @route	GET api/upload
-// @desc	Show form
+// @desc	Show upload form
 router.get("/", (req, res) => {
 	res.render("index");
 });
 
 // @route   POST api/upload-publication
 // @desc    Upload new publication
-router.post("/", upload.single("file"), (req, res) => {
-	//res.json({ file: req.file });
-	res.redirect("/api/upload");
+router.post("/", upload.array("file", 3), (req, res) => {
+	const { creator, contactPerson, contactEmail, title } = req.body;
+	const newPublication = new Publication({
+		creator,
+		contactPerson,
+		contactEmail,
+		title,
+		pictures: [],
+	});
+
+	req.files.map((file) => {
+		newPublication.pictures.push(
+			"http://localhost:8000/api/images/" + file.filename
+		);
+	});
+
+	newPublication.save().then((publication) => {
+		res.json(publication);
+	});
+	//res.redirect("/api/upload");
 });
 
 module.exports = router;
